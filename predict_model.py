9edda02620feabe25793f7be76436522a1f9d406
yuki.ieiri <yuki.ieiri@brainpad.co.jp>
2018-06-22 11:57:17 +0900

    add predict_model.py

---
+++ predict_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from sklearn.linear_morel import Logistic_Regression as logis
+from sklearn.model_selection import train_test_split
+import sklearn.preprocessing as prp
+
+mdl = Logistic_Regression()
