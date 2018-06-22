4f7ef0a87a5bebcf4195772f74809e600feac1b8
yuki.ieiri <yuki.ieiri@brainpad.co.jp>
2018-06-22 14:21:29 +0900

    using pclass and sex

--- predict_model.py
+++ predict_model.py
@@ -3,5 +3,21 @@
 from sklearn.linear_morel import Logistic_Regression as logis
 from sklearn.model_selection import train_test_split
 import sklearn.preprocessing as prp
+import pandas as pd
+
+data0 = pd.read_csv("./data/train.csv", sep=",", header=0)
+
+data1 = data0[['Survived','Pclass','Sex']]
 
 mdl = Logistic_Regression()
+
+x_train, y_train, x_test, y_test = train_test_split(
+    data0[['Pclass','Sex']], data0[['Survived']], test_size=0.2
+)
+
+mdl.fit(x_train, y_train)
+
+print(mdl.coef_)
+print(mdl.intercept_)
+
+#end of file
--- processing.py
+++ processing.py
@@ -2,35 +2,31 @@
 
 from sklearn import preprocessing as prp
 import pandas as pd
-import numpy as np
-
-a = 1.
-b = 3.
-c = a * b
-print("{0}".format(c))
 
 data = pd.read_csv("./data/train.csv", sep=",", header=0)
 
-print(data.describe())
+print("description: ", data.describe())
 print("data shape is: {0}".format(data.shape))
+
+print(data['Sex'].dtypes)
+
+data = data.mask(data == 'female', 1)
+data = data.mask(data == 'male', 0)
+data['Sex'] = data['Sex'].astype('int64')
+
+print(data['Sex'].dtypes)
 
 print("{0}".format(data.loc[1]))
 print("{0}".format(data.columns))
-print(data.loc[:,['Pclass','Age','Fare']].corr())
+print(data.iloc[:,1:12].corr())
 
-'''
 for col in data.columns:
     nacount = sum(pd.isna(data[col]))
     print("{0} has na: {1}".format(col, nacount))
-'''
 
 train_data = data[['Survived','Pclass','Sex']]
 print("shape changed: {0}".format(train_data.shape))
 
-
-sex = train_data['Sex']
-print(sex == 'female')
-
-sex = sex[]
+#print(train_data['Sex'])
 
 #end of file
