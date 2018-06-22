fd33b976cbf0184b1e2e3f9ebbe5545e6d5d218d
yuki.ieiri <yuki.ieiri@brainpad.co.jp>
2018-06-22 16:10:22 +0900

    created test prediction and submission csv

--- predict_test.py
+++ predict_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from time import time
 import pandas as pd
 
@@ -8,8 +9,12 @@ train0 = pd.read_csv("./data/train.csv", sep=",", header=0)
 test0 = pd.read_csv("./data/test.csv", sep=",", header=0)
 
 train1 = train0[['Survived','Pclass','Sex', 'Parch']]
-train1 = data1.mask(train1 == 'female', 1)
-train1 = data1.mask(train1 == 'male', 0)
+train1 = train1.mask(train1 == 'female', 1)
+train1 = train1.mask(train1 == 'male', 0)
+
+test1 = test0[['Pclass','Sex', 'Parch']]
+test1 = test1.mask(test1 == 'female', 1)
+test1 = test1.mask(test1 == 'male', 0)
 
 mdl = LogisticRegression(C=0.2)
 
@@ -22,10 +27,11 @@ mdl.fit(x_train, y_train)
 print("score for train is: {0}".format(mdl.score(x_train, y_train)))
 print("score for test is: {0}".format(mdl.score(x_test, y_test)))
 
+pred = mdl.predict(test1[['Pclass','Sex', 'Parch']])
 
-'''
-for i in range(5):
+submit = pd.DataFrame({'PassengerId': test0['PassengerId'],
+                        'Survived': pred})
 
-'''
+submit.to_csv("./data/pc_sx_pr_submission.csv", index=False)
 
 #end of file
